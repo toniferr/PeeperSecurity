f9188c2bd33d3e0d6971ce6fd2b6f957c0211f5d
toni <ferreirocouto@gmail.com>
2021-07-21 21:15:46 +0200

    hash md5 and salt value

---
+++ src/test/java/toni/ferreiro/brewery/web/controllers/PasswordEncodingTest.java
@@ -0,0 +1,18 @@
+package toni.ferreiro.brewery.web.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.util.DigestUtils;
+
+public class PasswordEncodingTest {
+
+    static final String PASSWORD = "password";
+
+    @Test
+    void hashingExample() {
+        System.out.println(DigestUtils.md5DigestAsHex(PASSWORD.getBytes()));
+        System.out.println(DigestUtils.md5DigestAsHex(PASSWORD.getBytes()));
+
+        String salted = PASSWORD + "ThisIsMySALTVALUE";
+        System.out.println(DigestUtils.md5DigestAsHex(salted.getBytes()));
+    }
+}
