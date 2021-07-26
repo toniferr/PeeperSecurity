5103c4a5f62182bd4916b6470d7588a234be0ddd
toni <ferreirocouto@gmail.com>
2021-07-26 18:01:22 +0200

    custom auth filter

--- src/test/java/toni/ferreiro/brewery/web/controllers/api/BeerRestControllerIT.java
+++ src/test/java/toni/ferreiro/brewery/web/controllers/api/BeerRestControllerIT.java
@@ -4,11 +4,19 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import toni.ferreiro.brewery.web.controllers.BaseIT;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
 public class BeerRestControllerIT extends BaseIT {
+
+    @Test
+    void deleteBeer() throws Exception {
+        mockMvc.perform(delete("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311")
+                .header("Api-Key", "spring").header("Api-Secret", "toni"))
+                .andExpect(status().isOk());
+    }
 
     @Test
     void findBeers() throws Exception{
