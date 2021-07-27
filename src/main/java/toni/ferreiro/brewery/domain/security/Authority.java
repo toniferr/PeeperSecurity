a50c1d5482673ab2ea2e6781a668ddb30a40a548
toni <ferreirocouto@gmail.com>
2021-07-27 18:19:08 +0200

    entities database auth

---
+++ src/main/java/toni/ferreiro/brewery/domain/security/Authority.java
@@ -0,0 +1,17 @@
+package toni.ferreiro.brewery.domain.security;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Authority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String role;
+
+    @ManyToMany(mappedBy = "authorities")
+    private Set<User> users;
+}
